e28187fdf80fb0f3fbec5952f43b6fa4b27a8127
Anishojha006 <anishojha1512@gmail.com>
2026-07-04 19:11:01 +0530

    working on  green and yellow circles functionality

--- src/components/folders/Folder.jsx
+++ src/components/folders/Folder.jsx
@@ -6,17 +6,17 @@ const folder = () => {
     <div className='folder'>
         <div className='folder-continer'>
          <img src="public/archive_3804719.png" />
-           <h3>Sun down wbsite</h3>
+           <h3>Projects</h3>
         </div>
 
          <div className='folder-continer'>
          <img src="public/archive_3804719.png" />
-           <h3>Sun down wbsite</h3>
+           <h3>Comments</h3>
         </div>
 
          <div className='folder-continer'>
          <img src="public/archive_3804719.png" />
-           <h3>Sun down wbsite</h3>
+           <h3>Like</h3>
         </div>
 
          <div className='folder-continer'>
