b2e5b41c16a6d7b410ef02aa5db6e4b5e3bb1fc4
Anishojha006 <anishojha1512@gmail.com>
2026-07-08 19:51:05 +0530

    working on input

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MacWindow from './windows/MacWindow'
 import "./comments.scss"
 
 const Comments = () => {
+  
+  const [inputName, setinputName] = useState("");
+  const [inputMessage, setinputMessage] = useState("");
+
+  const nameHandler =(e)=>{
+    setinputName(e.target.value)
+  }
+  const messageHandler =(e)=>{
+  setinputMessage(e.target.value);
+  }
   return (
     <MacWindow>
         <div className='comments-section'>
@@ -10,8 +20,8 @@ const Comments = () => {
             <h2>If you really enjoyed this experience. Consider leaving a comment!</h2>
             <h2>I would love to hear from you!</h2>
             <h3></h3>
-            <input type="text" placeholder='Name' />
-            <input type="text"  placeholder='Message'/>
+            <input type="text" value={inputName} placeholder='Name' onChange={nameHandler} />
+            <input type="text" value={inputMessage}  placeholder='Message' onChange={messageHandler}/>
 
             <button>
               <span>Send</span>
