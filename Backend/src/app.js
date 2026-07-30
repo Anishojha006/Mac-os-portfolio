980f7333cf276e27619cae0617bc9a3f0db4d4f8
Anishojha006 <anishojha1512@gmail.com>
2026-07-30 15:21:39 +0530

    fixing issues in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,14 @@ const app = express(); // creating an instance of server
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+app.use(express.static("./public"))
 const commentRoute = require("./routes/comment.routes");
 
 app.use("/api",commentRoute);
+// __dirname give path of my file in which this code is written 
+// * is wild card route  =>helps to handel those api's which are not created by us
+app.use('*name',(req,res)=>{
+  res.sendFile(path.join(__dirname, ".." , "/public/index.html" ))
+})
+
 module.exports = app;
